b83b72a968b049db589501484ef29019be66a4ea
kim <s158614@nishitech.ac.jp>
2019-11-21 17:08:57 +0900

    Add user model

---
+++ models/users.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true 
+    },
+    avatar : {
+        type : String,
+        required : true
+    },
+    date : {
+        type : String,
+        default : Date.now
+    }
+
+});
+
+
+module.exports = mongoose.model("user", userSchema);
