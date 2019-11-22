20f38985ef4a1809db4835b94d8d64f14ce660d6
kim <s158614@nishitech.ac.jp>
2019-11-22 15:25:09 +0900

    Controller (users)

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 
 const userModel = require("../models/users");
@@ -57,4 +58,79 @@ exports.user_register = (req, res) => {
                 error : err.message
             });
         });
+};
+
+
+
+
+
+
+//로그인
+exports.user_get_login = (req, res) => {
+
+    userModel
+        .findOne({email : req.body.email})
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({
+                    msg : "No emailInfo"
+                });
+            }
+            else {
+                // 패스워드 매칭
+                bcrypt
+                    .compare(req.body.password, user.password)
+                    .then(isMatch => {
+                        if (!isMatch) {
+                            return res.status(400).json({
+                                msg : "password incorrect"
+                            });
+                        }
+                        else {
+                            //토큰에 들어갈 유저정
+                            const payload = {
+                                id : user.id,
+                                name : user.name,
+                                avatar : user.avatar
+                            };
+                            // sign token
+                            jwt.sign(
+                                payload,
+                                process.env.JWT_SECRET,
+                                { expiresIn : 36000 }, //만료시간
+                                (err, token) => {
+                                    res.status(200).json({
+                                        msg : "successful login(토큰 반환)",
+                                        tokenInfo : "bearer " + token
+                                    });
+                                }
+                            );
+                        }
+                    })
+                    .catch(err => {
+                        res.status(404).json({
+                            error : err.message
+                        });
+                    });
+
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error : err.message
+            });
+        });
+
+};
+
+
+
+
+//회원정보 불러오기
+exports.user_get_all = (req, res) => {
+    res.status(200).json({
+        id : req.user.id,
+        name : req.user.name,
+        avatar : req.user.avatar
+    })
 };
--- routes/users.js
+++ routes/users.js
@@ -20,62 +20,7 @@ router.post("/register", userController.user_register);
 // @route   POST http://localhost:1234/users/login
 // @desc    user login / return jsonwebtoken
 // @access  public
-router.post("/login", (req, res) => {
-
-    userModel
-        .findOne({email : req.body.email})
-        .then(user => {
-            if (!user) {
-                return res.status(404).json({
-                    msg : "No emailInfo"
-                });
-            }
-            else {
-                // 패스워드 매칭
-                bcrypt
-                    .compare(req.body.password, user.password)
-                    .then(isMatch => {
-                        if (!isMatch) {
-                            return res.status(400).json({
-                                msg : "password incorrect"
-                            });
-                        }
-                        else {
-                            //토큰에 들어갈 유저정
-                            const payload = {
-                                id : user.id,
-                                name : user.name,
-                                avatar : user.avatar
-                            };
-                            // sign token
-                            jwt.sign(
-                                payload,
-                                process.env.JWT_SECRET,
-                                { expiresIn : 36000 }, //만료시간
-                                (err, token) => {
-                                    res.status(200).json({
-                                        msg : "successful login(토큰 반환)",
-                                        tokenInfo : "bearer " + token
-                                    });
-                                }
-                            );
-                        }
-                    })
-                    .catch(err => {
-                        res.status(404).json({
-                            error : err.message
-                        });
-                    });
-
-            }
-        })
-        .catch(err => {
-            res.status(500).json({
-                error : err.message
-            });
-        });
-
-});
+router.post("/login", userController.user_get_login);
 
 
 // @route   DELETE http://localhost:1234/users/:user_id
@@ -90,13 +35,7 @@ router.delete("/:user_id", (req, res) => {
 // @route   GET http://localhost:1234/users/
 // @desc    get userInfo
 // @access  private
-router.get("/", auth_check, (req, res) => {
-    res.status(200).json({
-        id : req.user.id,
-        name : req.user.name,
-        avatar : req.user.avatar
-    })
-});
+router.get("/", auth_check, userController.user_get_all);
 
 
 
