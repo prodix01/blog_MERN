bc64cb5357a54839f75cb6377a9b883fe6cffe47
kim <s158614@nishitech.ac.jp>
2019-11-27 10:38:16 +0900

    Add profile Model

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,22 +1,33 @@
 const express = require("express");
+const passport = require("passport");
 const router = express.Router();
 
+const userModel = require("../models/users");
+const profileModel = require("../models/profiles");
+
+const auth_check = passport.authenticate("jwt", {session : false});
 
 // 프로필 등록
+router.post("/", auth_check, (req, res) => {
 
+
+
+
+});
 
 
 
 //프로필 불러오기
+router.get("/", auth_check, (req, res) => {
 
-
-
-//프로필 수정
+});
 
 
 
 //프로필 삭제
+router.delete("/", auth_check, (req, res) => {
 
+});
 
 
 module.exports = router;
