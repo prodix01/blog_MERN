951723be783cff6782e3671643e30a933f47c506
kim <s158614@nishitech.ac.jp>
2019-12-02 12:19:28 +0900

    Add post C.R.D

--- models/posts.js
+++ models/posts.js
@@ -42,7 +42,7 @@ const postSchema = mongoose.Schema({
             },
             date : {
                 type : Date,
-                default : Date.now 
+                default : Date.now
             }
 
         }
