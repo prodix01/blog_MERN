9730fc55a5271d0d70dea7bd2f13a366401e3b0e
kim <s158614@nishitech.ac.jp>
2019-12-17 09:12:29 +0900

    Navbar #2 (add buttons)

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import React, {Component} from 'react';
 import Footer from "./componets/layout/Footer";
 import Navbar from "./componets/layout/Navbar";
 
+
 class App extends Component {
   render() {
     return (
