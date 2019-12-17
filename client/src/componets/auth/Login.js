b67621befe155cf0d71be3ec7a35ec201cb3bb99
kim <s158614@nishitech.ac.jp>
2019-12-17 10:26:44 +0900

    create Register form

--- client/src/componets/auth/Login.js
+++ client/src/componets/auth/Login.js
@@ -3,8 +3,15 @@ import React, {Component} from 'react';
 class Login extends Component {
     render() {
         return (
-            <div>
-                <h1>Login</h1>
+            <div className="login">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 md-auto">
+                            <h1 className="display-4 text-center">Login</h1>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
